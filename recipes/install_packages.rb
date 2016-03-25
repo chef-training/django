3d7131843e55c5b7e5ba73d24e2afe05d195d75c
Franklin Webber <franklin.webber@gmail.com>
2016-03-25 12:16:43 -0500

    Implement installation by package
    Installs django with the package name.

--- recipes/install_packages.rb
+++ recipes/install_packages.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: django
-# Recipe:: default
+# Recipe:: install_packages.rb
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-include_recipe 'django::install_packages'
+package 'python-django'
